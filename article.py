27d7f7f79e8a329f6f73285a65ac0a08b829a0e8
lisztomania <lisztomania@vip.qq.com>
2020-12-12 14:33:24 +0800

    文章类

---
+++ article.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author   : lisztomania
+# @Date     : 2020/12/10
+# @Software : Pycharm
+# @Version  : Python 3.8.2
+# @File     : article.py
+# @Function : 文章类
+from task import Task
+
+
+class Article(Task):
+    pass
