afef3b6e0f0d624549a7a075d2eb2e747484d791
lisztomania <lisztomania@vip.qq.com>
2021-02-04 21:54:11 +0800

    增加答题失败类

---
+++ inside/Template/Task_Exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author   : lisztomania
+# @Date     : 2021/2/4
+# @Software : Pycharm
+# @Version  : Python 3.8.5
+# @File     : Task_Exception.py
+# @Function : 任务异常
+
+class TASK_EXCEPTION(Exception):
+    pass
