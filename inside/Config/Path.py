9e2a023917b86460fb02984aed9fe638c3d38dd4
lisztomania <lisztomania@vip.qq.com>
2021-02-08 18:12:01 +0800

    进测试在Mac10上，似乎不存在路径问题 我现在搞不清Mac的文件方式了 现在更改为绝对路径模式

--- inside/Config/Path.py
+++ inside/Config/Path.py
@@ -93,13 +93,10 @@ class PATH(metaclass=SINGLETON):
 
         :return: str
         """
-        xt = SYSTEM().System
         temp = os.path.join(os.path.dirname(self.__Config_Path), 'Task')
         temp = os.path.join(temp, 'Mitmdump')
         temp = os.path.join(temp, 'Intercept')
         temp = os.path.join(temp, 'Script.py')
-        if xt == 'macOs':
-            temp = temp.replace(os.path.expanduser('~'), '~')
         return temp
 
     @property
